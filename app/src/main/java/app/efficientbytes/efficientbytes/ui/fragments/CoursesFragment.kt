3bcb535be61f12e98406513479875f3e58366479
Anubhav_PS <74093939+Anubhav-PS@users.noreply.github.com>
2024-02-17 00:55:00 +0530

    Implementation of courses fragment base design (#4)
    Navigation from course fragment to account fragment is implemented

--- app/src/main/java/app/efficientbytes/efficientbytes/ui/fragments/CoursesFragment.kt
+++ app/src/main/java/app/efficientbytes/efficientbytes/ui/fragments/CoursesFragment.kt
@@ -2,23 +2,53 @@ package app.efficientbytes.efficientbytes.ui.fragments
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import app.efficientbytes.efficientbytes.R
+import app.efficientbytes.efficientbytes.databinding.FragmentCoursesBinding
 
 class CoursesFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private val tagCoursesFragment: String = "View-Byte-Course-Fragment"
+    private lateinit var _binding: FragmentCoursesBinding
+    private val binding get() = _binding
+    private lateinit var rootView: View
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_courses, container, false)
+    ): View {
+        _binding = FragmentCoursesBinding.inflate(inflater, container, false)
+        rootView = binding.root
+        return rootView
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        (requireActivity() as MenuHost).addMenuProvider(object : MenuProvider {
+            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                menuInflater.inflate(R.menu.toolbar_account_menu, menu)
+            }
+
+            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                when (menuItem.itemId) {
+                    R.id.accountSettingsMenu -> {
+                        view.findNavController()
+                            .navigate(R.id.coursesFragment_to_accountSettingsFragment)
+                        return true
+                    }
+                }
+                return false
+            }
+        }, viewLifecycleOwner)
+
     }
 
 }
