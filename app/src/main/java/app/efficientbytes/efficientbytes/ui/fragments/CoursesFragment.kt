45dcf06b6a09e73b0ae7aef257e9841b4cb7d321
Anubhav-PS <anubhav.p.sahoo@gmail.com>
2024-02-17 13:33:18 +0530

    Implemented courses content type filter using chips

--- app/src/main/java/app/efficientbytes/efficientbytes/ui/fragments/CoursesFragment.kt
+++ app/src/main/java/app/efficientbytes/efficientbytes/ui/fragments/CoursesFragment.kt
@@ -7,11 +7,13 @@ import android.view.MenuInflater
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.core.view.MenuHost
 import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import app.efficientbytes.efficientbytes.R
+import app.efficientbytes.efficientbytes.databinding.ChipCoursesFilterBinding
 import app.efficientbytes.efficientbytes.databinding.FragmentCoursesBinding
 
 class CoursesFragment : Fragment() {
@@ -20,6 +22,7 @@ class CoursesFragment : Fragment() {
     private lateinit var _binding: FragmentCoursesBinding
     private val binding get() = _binding
     private lateinit var rootView: View
+    private var coursesFilterIdMapping: HashMap<Int, String>? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -48,7 +51,31 @@ class CoursesFragment : Fragment() {
                 return false
             }
         }, viewLifecycleOwner)
+        populateChipGroup()
+        binding.coursesContentTypeFilterChipGroup.setOnCheckedStateChangeListener { group, _ ->
+            val contentType = coursesFilterIdMapping?.get(group.checkedChipId)
+            Toast.makeText(
+                requireContext(),
+                "The content type is : $contentType",
+                Toast.LENGTH_LONG
+            ).show()
+        }
+    }
 
+    private fun populateChipGroup() {
+        val inflater = LayoutInflater.from(requireContext())
+        val filterList = resources.getStringArray(R.array.array_course_filters)
+        coursesFilterIdMapping = HashMap()
+        var id = 1;
+        filterList.forEach {
+            val chipCoursesFilterBinding: ChipCoursesFilterBinding =
+                ChipCoursesFilterBinding.inflate(inflater)
+            chipCoursesFilterBinding.name = it
+            chipCoursesFilterBinding.isChecked = id == 1
+            coursesFilterIdMapping?.set(id, it)
+            chipCoursesFilterBinding.root.id = id++
+            binding.coursesContentTypeFilterChipGroup.addView(chipCoursesFilterBinding.root)
+        }
     }
 
 }
