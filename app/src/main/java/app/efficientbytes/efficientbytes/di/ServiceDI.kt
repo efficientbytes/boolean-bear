8ba97aad82dedda78268eff3d8fa1d261319df93
Anubhav-PS <anubhav.p.sahoo@gmail.com>
2024-02-18 13:16:19 +0530

    Courses Service class and repository

--- app/src/main/java/app/efficientbytes/efficientbytes/di/ServiceDI.kt
+++ app/src/main/java/app/efficientbytes/efficientbytes/di/ServiceDI.kt
@@ -4,7 +4,6 @@ import app.efficientbytes.efficientbytes.services.CoursesService
 import app.efficientbytes.efficientbytes.utils.BASE_URL
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.create
 
 const val baseUrl = BASE_URL
 
