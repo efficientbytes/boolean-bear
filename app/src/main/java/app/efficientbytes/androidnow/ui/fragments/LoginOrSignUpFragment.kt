69e104497db4a0333e34299f8c0ceb5c05f605ee
Anubhav_PS <74093939+Anubhav-PS@users.noreply.github.com>
2024-02-29 01:00:28 +0530

    Login or Sign up code implementation (#23)
    Login or Sign up code implementation

--- app/src/main/java/app/efficientbytes/androidnow/ui/fragments/LoginOrSignUpFragment.kt
+++ app/src/main/java/app/efficientbytes/androidnow/ui/fragments/LoginOrSignUpFragment.kt
@@ -1,6 +1,8 @@
 package app.efficientbytes.androidnow.ui.fragments
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,10 +10,11 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import app.efficientbytes.androidnow.R
 import app.efficientbytes.androidnow.databinding.FragmentLoginOrSignUpBinding
+import app.efficientbytes.androidnow.utils.validatePhoneNumberFormat
 
 class LoginOrSignUpFragment : Fragment() {
 
-    private val tagCoursesFragment: String = "Login-Sign-Up-Fragment"
+    private val tagLoginOrSignUpFragment: String = "Login-Sign-Up-Fragment"
     private lateinit var _binding: FragmentLoginOrSignUpBinding
     private val binding get() = _binding
     private lateinit var rootView: View
@@ -29,8 +32,25 @@ class LoginOrSignUpFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.continueButton.setOnClickListener {
-            it.findNavController().navigate(R.id.loginOrSignUpFragment_to_OTPVerificationFragment)
+            val input = binding.phoneNumberTextInputEditText.text.toString()
+            if (validatePhoneNumberFormat(binding.phoneNumberTextInputLayout,input)) {
+                binding.phoneNumberTextInputEditText.text=null
+                //make server call to verify by adding prefix
+                it.findNavController().navigate(R.id.loginOrSignUpFragment_to_OTPVerificationFragment)
+            }
         }
+        binding.phoneNumberTextInputEditText.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                binding.phoneNumberTextInputLayout.error = null
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+            }
+
+        })
     }
 
 }
