0b04d54cdc8febb00fba788fd6fcf8f153d22d1f
Anubhav-PS <anubhav.p.sahoo@gmail.com>
2024-03-01 22:00:00 +0530

    View model implementation

--- app/src/main/java/app/efficientbytes/androidnow/di/ViewModelModule.kt
+++ app/src/main/java/app/efficientbytes/androidnow/di/ViewModelModule.kt
@@ -1,9 +1,13 @@
 package app.efficientbytes.androidnow.di
 
 import app.efficientbytes.androidnow.viewmodels.CourseViewModel
+import app.efficientbytes.androidnow.viewmodels.LoginOrSignUpViewModel
+import app.efficientbytes.androidnow.viewmodels.PhoneNumberOTPVerificationViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val viewModelModule = module {
     viewModel { CourseViewModel(get()) }
+    viewModel { LoginOrSignUpViewModel(get()) }
+    viewModel { PhoneNumberOTPVerificationViewModel(get()) }
 }
