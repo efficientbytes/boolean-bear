ceeef805d1219619acf9eaebb89e52c28531d9bb
Anubhav-PS <anubhav.p.sahoo@gmail.com>
2024-03-13 16:59:00 +0530

    Fixed crash associated with connectivity listener

--- app/src/main/java/app/efficientbytes/androidnow/utils/ConnectivityListener.kt
+++ app/src/main/java/app/efficientbytes/androidnow/utils/ConnectivityListener.kt
@@ -6,14 +6,18 @@ import android.content.Intent
 import android.net.ConnectivityManager
 import android.net.Network
 import android.net.NetworkInfo
+import android.util.Log
 import androidx.lifecycle.LiveData
 
 class ConnectivityListener(context: Context) :
     LiveData<Boolean>() {
 
-    private val connectivityManager: ConnectivityManager =
-        context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-    private lateinit var networkConnectionCallback: ConnectivityManager.NetworkCallback
+    private var connectivityManager: ConnectivityManager? = null
+    private var networkConnectionCallback: ConnectivityManager.NetworkCallback? = null
+
+    init{
+        connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+    }
 
     private fun connectivityManagerCallback(): ConnectivityManager.NetworkCallback {
         networkConnectionCallback = object : ConnectivityManager.NetworkCallback() {
@@ -27,11 +31,11 @@ class ConnectivityListener(context: Context) :
                 postValue(true)
             }
         }
-        return networkConnectionCallback
+        return networkConnectionCallback as ConnectivityManager.NetworkCallback
     }
 
     private fun updateNetworkConnection() {
-        val activeNetworkConnection: NetworkInfo? = connectivityManager.activeNetworkInfo
+        val activeNetworkConnection: NetworkInfo? = connectivityManager?.activeNetworkInfo
         postValue(activeNetworkConnection?.isConnected == true)
     }
 
@@ -44,16 +48,23 @@ class ConnectivityListener(context: Context) :
     override fun onActive() {
         super.onActive()
         updateNetworkConnection()
-        connectivityManager.registerDefaultNetworkCallback(connectivityManagerCallback())
+        connectivityManager?.registerDefaultNetworkCallback(connectivityManagerCallback())
     }
 
     override fun onInactive() {
         super.onInactive()
-        connectivityManager.unregisterNetworkCallback(connectivityManagerCallback())
+        if (networkConnectionCallback!=null&&connectivityManager!=null){
+            try{
+                connectivityManager?.unregisterNetworkCallback(connectivityManagerCallback())
+                networkConnectionCallback = null
+            }catch (e : Exception){
+                Log.i("Connectivity Listener","Error is ${e.message}")
+            }
+        }
     }
 
     fun isInternetAvailable(): Boolean {
-        val internetAvailability: NetworkInfo? = connectivityManager.getActiveNetworkInfo()
+        val internetAvailability: NetworkInfo? = connectivityManager?.getActiveNetworkInfo()
         return internetAvailability != null && internetAvailability.isAvailable && internetAvailability.isConnected
     }
 
