6eebf3687c7c5de2f1dfea8dba3106b68fd9c864
Anubhav-PS <anubhav.p.sahoo@gmail.com>
2024-03-17 22:08:46 +0530

    Share feedback view model

--- app/src/main/java/app/efficientbytes/androidnow/di/ViewModelModule.kt
+++ app/src/main/java/app/efficientbytes/androidnow/di/ViewModelModule.kt
@@ -7,6 +7,7 @@ import app.efficientbytes.androidnow.viewmodels.EditProfileFieldViewModel
 import app.efficientbytes.androidnow.viewmodels.LoginOrSignUpViewModel
 import app.efficientbytes.androidnow.viewmodels.MainViewModel
 import app.efficientbytes.androidnow.viewmodels.PhoneNumberOTPVerificationViewModel
+import app.efficientbytes.androidnow.viewmodels.ShareFeedbackViewModel
 import org.koin.android.ext.koin.androidApplication
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
@@ -19,4 +20,5 @@ val viewModelModule = module {
     viewModel { MainViewModel(androidApplication(), get(), get(), get()) }
     viewModel { AccountSettingsViewModel(get()) }
     viewModel { EditProfileFieldViewModel(get(), get()) }
+    viewModel { ShareFeedbackViewModel(get()) }
 }
