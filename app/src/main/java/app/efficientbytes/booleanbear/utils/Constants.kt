f27a45e96bedc74586b58779bd6392f6d774b910
Anubhav-PS <anubhav.p.sahoo@gmail.com>
2024-03-28 17:27:47 +0530

    Renamed the base url

--- app/src/main/java/app/efficientbytes/booleanbear/utils/Constants.kt
+++ app/src/main/java/app/efficientbytes/booleanbear/utils/Constants.kt
@@ -1,6 +1,6 @@
 package app.efficientbytes.booleanbear.utils
 
-const val BASE_URL = "https://androidnow-zl4cootq3q-uc.a.run.app/"
+const val BASE_URL = "https://booleanbear-fb7pgif5zq-uc.a.run.app"
 const val DATABASE_NAME = "APP_DATABASE"
 const val DUMMY_TABLE = "dummy"
 const val USER_PROFILE_TABLE = "user_profile"
