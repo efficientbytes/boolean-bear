8970c56acf798de7adc4dd899ef9e87c60bce629
Anubhav-PS <anubhav.p.sahoo@gmail.com>
2024-04-03 14:54:44 +0530

    Service for getting contents

--- app/src/main/java/app/efficientbytes/booleanbear/services/AssetsService.kt
+++ app/src/main/java/app/efficientbytes/booleanbear/services/AssetsService.kt
@@ -1,12 +1,29 @@
 package app.efficientbytes.booleanbear.services
 
 import app.efficientbytes.booleanbear.services.models.ContentCategoriesStatus
+import app.efficientbytes.booleanbear.services.models.ShuffledContentIds
+import app.efficientbytes.booleanbear.services.models.YoutubeContentViewStatus
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface AssetsService {
 
-    @GET("content/categories")
-    suspend fun getContentCategories(): Response<ContentCategoriesStatus>
+    @GET("categories")
+    suspend fun getCategories(
+        @Query(value = "categoryType") categoryType: String = "shuffled"
+    ): Response<ContentCategoriesStatus>
+
+    @GET("categories/shuffled/{categoryId}")
+    suspend fun getContentIdsUnderShuffledCategoryForCategoryId(
+        @Path("categoryId") categoryId: String
+    ): Response<ShuffledContentIds>
+
+    @GET("contents/{contentId}")
+    suspend fun getYoutubeTypeContentForContentId(
+        @Path("contentId") contentId: String,
+        @Query(value = "viewType") viewType: String = "YOUTUBE"
+    ): Response<YoutubeContentViewStatus>
 
 }
