3c64d47756d2022e9a7238be5c5d604f4872c366
Anubhav-PS <anubhav.p.sahoo@gmail.com>
2024-04-10 18:24:44 +0530

    Complete user profile fragment

--- app/src/main/java/app/efficientbytes/booleanbear/ui/fragments/CompleteProfileFragment.kt
+++ app/src/main/java/app/efficientbytes/booleanbear/ui/fragments/CompleteProfileFragment.kt
@@ -186,11 +186,24 @@ class CompleteProfileFragment : Fragment() {
                     findNavController().popBackStack(R.id.homeFragment, false)
                 }
 
-                DataStatus.Status.EmptyResult -> {}
-                DataStatus.Status.NoInternet -> {}
-                DataStatus.Status.TimeOut -> {}
-                DataStatus.Status.UnAuthorized -> {}
-                DataStatus.Status.UnKnownException -> {}
+                DataStatus.Status.NoInternet -> {
+                    binding.submitButton.isEnabled = true
+                    binding.progressBar.visibility = View.GONE
+                    binding.progressStatusValueTextView.visibility = View.VISIBLE
+                    binding.progressStatusValueTextView.text = "No Internet Connection."
+                }
+
+                DataStatus.Status.TimeOut -> {
+                    binding.submitButton.isEnabled = true
+                    binding.progressBar.visibility = View.GONE
+                    binding.progressStatusValueTextView.visibility = View.VISIBLE
+                    binding.progressStatusValueTextView.text =
+                        "Updating profile is taking unsually long time. Please try submitting it again."
+                }
+
+                else -> {
+
+                }
             }
         }
     }
