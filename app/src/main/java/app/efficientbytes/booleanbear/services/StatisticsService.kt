3572f7cd2ba89d8af89e400a8a42541b83f7d0c8
Anubhav-PS <anubhav.p.sahoo@gmail.com>
2024-04-24 17:21:12 +0530

    Implemented form url encoded in services

--- app/src/main/java/app/efficientbytes/booleanbear/services/StatisticsService.kt
+++ app/src/main/java/app/efficientbytes/booleanbear/services/StatisticsService.kt
@@ -4,22 +4,29 @@ import app.efficientbytes.booleanbear.database.models.ScreenTimingPerDay
 import app.efficientbytes.booleanbear.services.models.RequestContentCount
 import app.efficientbytes.booleanbear.services.models.ResponseMessage
 import retrofit2.Response
-import retrofit2.http.Body
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.Headers
 import retrofit2.http.POST
 import retrofit2.http.Path
 
 interface StatisticsService {
 
+    @FormUrlEncoded
     @POST("statistics/screen-timing/app/{userAccountId}/")
+    @Headers("Content-Type: application/x-www-form-urlencoded")
     suspend fun uploadScreenTimings(
         @Path("userAccountId") userAccountId: String,
-        @Body screenTimingPerDayList: List<ScreenTimingPerDay>
+        @Field("screenTimingPerDayList") screenTimingPerDayList: List<ScreenTimingPerDay>
     ): Response<ResponseMessage>
 
+    @FormUrlEncoded
     @POST("statistics/content-views/app/{contentId}/")
+    @Headers("Content-Type: application/x-www-form-urlencoded")
     suspend fun increaseContentViewCount(
         @Path("contentId") contentId: String,
-        @Body requestContentCount: RequestContentCount
+        @Field("userAccountId") userAccountId: String? = null,
+        @Field("message") message: String? = null
     ): Response<RequestContentCount>
 
 }
