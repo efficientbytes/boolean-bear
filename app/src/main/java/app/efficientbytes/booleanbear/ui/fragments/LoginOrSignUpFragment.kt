3541c65f5702fc5e5cd80c4bd941095802040007
Anubhav-PS <anubhav.p.sahoo@gmail.com>
2024-04-25 20:22:13 +0530

    Implemented terms and conditions

--- app/src/main/java/app/efficientbytes/booleanbear/ui/fragments/LoginOrSignUpFragment.kt
+++ app/src/main/java/app/efficientbytes/booleanbear/ui/fragments/LoginOrSignUpFragment.kt
@@ -1,14 +1,22 @@
 package app.efficientbytes.booleanbear.ui.fragments
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.text.Editable
+import android.text.SpannableString
+import android.text.SpannableStringBuilder
 import android.text.TextWatcher
+import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
+import android.text.style.ForegroundColorSpan
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import app.efficientbytes.booleanbear.BuildConfig
+import app.efficientbytes.booleanbear.R
 import app.efficientbytes.booleanbear.databinding.FragmentLoginOrSignUpBinding
 import app.efficientbytes.booleanbear.repositories.models.DataStatus
 import app.efficientbytes.booleanbear.utils.validatePhoneNumberFormat
@@ -33,6 +41,52 @@ class LoginOrSignUpFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val policy = "By continuing, I agree to the Terms of Use & Privacy Policy"
+        val termsOfUseIndex = 30
+        val privacyPolicyIndex = 45
+        val spannableStringBuilder = SpannableStringBuilder(policy)
+        val termsOfUseClickableSpan = object : ClickableSpan() {
+            override fun onClick(widget: View) {
+                val link =
+                    "https://efficientbytes.notion.site/Boolean-Bear-Terms-of-use-803e636c627946e4b6fdefdbf23b9ede"
+                openLink(link)
+            }
+        }
+        spannableStringBuilder.setSpan(
+            termsOfUseClickableSpan,
+            termsOfUseIndex,
+            42,
+            SpannableString.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+        spannableStringBuilder.setSpan(
+            ForegroundColorSpan(requireContext().getColor(R.color.violet_800)),
+            termsOfUseIndex,
+            42,
+            SpannableString.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+        val privacyPolicyClickableSpan = object : ClickableSpan() {
+            override fun onClick(widget: View) {
+                val link =
+                    "https://efficientbytes.notion.site/Boolean-Bear-Privacy-Policy-b2f43ae39b8a4c5880ef2a1cbd811b15"
+                openLink(link)
+            }
+        }
+        spannableStringBuilder.setSpan(
+            privacyPolicyClickableSpan,
+            privacyPolicyIndex,
+            59,
+            SpannableString.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+        spannableStringBuilder.setSpan(
+            ForegroundColorSpan(requireContext().getColor(R.color.violet_800)),
+            privacyPolicyIndex,
+            59,
+            SpannableString.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+
+        binding.policyNConditionsLabelTextView.text = spannableStringBuilder
+        binding.policyNConditionsLabelTextView.movementMethod = LinkMovementMethod.getInstance()
+
         binding.continueButton.setOnClickListener {
             val input = binding.phoneNumberTextInputEditText.text.toString()
             if (validatePhoneNumberFormat(binding.phoneNumberTextInputLayout, input)) {
@@ -118,6 +172,12 @@ class LoginOrSignUpFragment : Fragment() {
                 phoneNumber
             )
         rootView.findNavController().navigate(directions)
+    }
+
+    private fun openLink(link: String) {
+        val uri = Uri.parse(link)
+        val intent = Intent(Intent.ACTION_VIEW, uri)
+        requireContext().startActivity(intent)
     }
 
 }
