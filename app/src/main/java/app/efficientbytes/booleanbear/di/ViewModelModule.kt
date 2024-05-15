343dea0a040a3367dff83b5c0299601504ad6ad3
Anubhav-PS <anubhav.p.sahoo@gmail.com>
2024-05-15 22:44:03 +0530

    View model

--- app/src/main/java/app/efficientbytes/booleanbear/di/ViewModelModule.kt
+++ app/src/main/java/app/efficientbytes/booleanbear/di/ViewModelModule.kt
@@ -2,6 +2,7 @@ package app.efficientbytes.booleanbear.di
 
 import app.efficientbytes.booleanbear.viewmodels.AccountSettingsViewModel
 import app.efficientbytes.booleanbear.viewmodels.CompleteProfileViewModel
+import app.efficientbytes.booleanbear.viewmodels.CourseWaitingListViewModel
 import app.efficientbytes.booleanbear.viewmodels.DiscoverViewModel
 import app.efficientbytes.booleanbear.viewmodels.EditProfileFieldViewModel
 import app.efficientbytes.booleanbear.viewmodels.HomeViewModel
@@ -41,4 +42,5 @@ val viewModelModule = module {
     viewModel { ReelPlayerViewModel(get(), get(), get(), get(), get(), get()) }
     viewModel { DiscoverViewModel(get(), get()) }
     viewModel { ListReelViewModel(get(), get()) }
+    viewModel { CourseWaitingListViewModel(get()) }
 }
