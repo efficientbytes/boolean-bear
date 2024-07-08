6680505892645ec6da846e7ea2626959094b0137
Anubhav-PS <anubhav.p.sahoo@gmail.com>
2024-07-09 04:17:16 +0530

    version code upgrade 3->4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         applicationId = "app.efficientbytes.booleanbear"
         minSdk = 24
         targetSdk = 34
-        versionCode = 3
+        versionCode = 4
         versionName = "1.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
