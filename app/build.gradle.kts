834235c4316bce21e4ac582955dc9dd855f7dbee
Anubhav-PS <anubhav.p.sahoo@gmail.com>
2024-07-14 21:26:11 +0530

    Upgraded version 1.5.4 -> 1.6.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "app.efficientbytes.booleanbear"
         minSdk = 24
         targetSdk = 34
-        versionCode = 9
-        versionName = "1.5.4"
+        versionCode = 10
+        versionName = "1.6.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
