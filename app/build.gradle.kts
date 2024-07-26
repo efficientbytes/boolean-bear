e9f7566921c787fddf2bd3c2e798956c4cd367cb
Anubhav-PS <anubhav.p.sahoo@gmail.com>
2024-07-26 14:10:56 +0530

    upgraded version 1.8.0 -> 1.9.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "app.efficientbytes.booleanbear"
         minSdk = 24
         targetSdk = 34
-        versionCode = 13
-        versionName = "1.8.0"
+        versionCode = 14
+        versionName = "1.9.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
