48c8bf47c1a85755184bb2d88d428e3c1fcd5222
Anubhav-PS <anubhav.p.sahoo@gmail.com>
2024-08-06 22:13:41 +0530

    Upgraded version 1.10.1 -> 1.10.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "app.efficientbytes.booleanbear"
         minSdk = 24
         targetSdk = 34
-        versionCode = 17
-        versionName = "1.10.1"
+        versionCode = 18
+        versionName = "1.10.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
